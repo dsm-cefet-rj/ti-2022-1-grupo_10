2cdda8734e987c6d8d0090d9bad23f4f1f14edc1
Lucca-1999 <72893497+Lucca-1999@users.noreply.github.com>
2022-05-18 14:10:52 -0300

    Update App.js
    Update de APP.js com a parte de cadastro materia prima

--- src/App.js
+++ src/App.js
@@ -5,41 +5,75 @@ import RelatorioMateriaPrima from "./Components/RelatorioMateriaPrima";
 import Cadastro from './Components/Cadastro';
 import Home from "./Components/Home";
 import Producao from "./Components/producao";
+import CadastroMateriaPrima from './Components/CadastroMateriaPrima';
 
 
 
 const App = () => {
   
   const [tableData, setTableData] = useState([])
+  const [tableMaterial, setTableMaterial] = useState([])
   const [formInputData, setformInputData] = useState(
       {
       nomeProduto:'',
-      qtdProduto:''
+      qtdProduto:'',
+      custoProduto:'',
+      valorProduto:'',
       }
   );
  
   const handleChange=(evnt)=>{  
-      const newInput = (data)=>({...data, [evnt.target.name]:evnt.target.value})
-      setformInputData(newInput)
+    const newInput = (data)=>({...data, [evnt.target.name]:evnt.target.value})
+    setformInputData(newInput)
   }
   
   const handleSubmit= (evnt) =>{
-      evnt.preventDefault();
-      const checkEmptyInput = !Object.values(formInputData).every(res=>res==="")
+    evnt.preventDefault();
+    const checkEmptyInput = !Object.values(formInputData).every(res=>res==="")
+    if(checkEmptyInput)
+    {
+      const newData = (data)=>([...data, formInputData])
+      setTableData(newData);
+      const emptyInput= {nomeProduto:'', qtdProduto:'', custoProduto:'', valorProduto:''}
+      setformInputData(emptyInput)
+    }
+  }
+
+  //Formulário da Matéria-prima
+  const [formInput, setformInput] = useState(
+    {
+      nomeMateriaPrima:'',
+      qtdMateriaPrima:'',
+      fornecedor:'',
+      valorMateriaPrima:'',
+    }
+    );
+
+    const onChange=(event)=>{  
+      const newInput = (data)=>({...data, [event.target.name]:event.target.value})
+      setformInput(newInput)
+    }
+
+    const onSubmit= (event) =>{
+      event.preventDefault();
+      const checkEmptyInput = !Object.values(formInput).every(res=>res==="")
       if(checkEmptyInput)
       {
-        const newData = (data)=>([...data, formInputData])
-        setTableData(newData);
-        const emptyInput= {nomeProduto:'', qtdProduto:''}
-        setformInputData(emptyInput)
+        const newData = (data)=>([...data, formInput])
+        setTableMaterial(newData);
+        const emptyInput= {nomeMateriaPrima:'', qtdMateriaPrima:'', fornecedor:'', valorMateriaPrima:''}
+        setformInput(emptyInput)
       }
   }
+  
   
   return(
   <>
      <main>
 
-        <Home tableData={tableData} />
+        <Home tableData={tableData} tableMaterial={tableMaterial} />
+
+        <CadastroMateriaPrima onChange={onChange} formInput={formInput} onSubmit={onSubmit}/>
       
         <Cadastro handleChange={handleChange} formInputData={formInputData} handleSubmit={handleSubmit}/>
   
@@ -58,7 +92,8 @@ const App = () => {
       </main>
   </>  
    
-  )
+  ) 
+
 }
 
 export default App;
