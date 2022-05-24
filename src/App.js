9a7424914854459552f774dd6786c90be9ab4658
Lucca-1999 <72893497+Lucca-1999@users.noreply.github.com>
2022-05-24 04:57:05 -0300

    Consertando rotas de navegação
    Tirando a barra de navegação redundante e colocando home como front page, além de consertar a WS do React

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import './App.css';
 import RelatorioProduzidos from "./Components/RelatorioProduzidos";
 import RelatorioMateriaPrima from "./Components/RelatorioMateriaPrima";
@@ -74,46 +74,20 @@ const App = () => {
   <>
      <main>
 
-       <Router>
-          <nav>
-            <Link to="/home"> Home </Link>
-            <Link to="/cadastro"> Cadastro </Link>
-            <Link to="/cadastroMP"> CadastroMateriaPrima </Link>
-            <Link to="/producao"> Produção </Link>
-            <Link to="/relatorioproduzidos"> Relatorioproduzidos </Link>
-            <Link to="/venda"> Venda </Link>
-            <Link to="/relatorioMP"> relatorioMatériaPrima </Link>
-          </nav>
-          
+
+       <Router>          
           <Routes>
-            <Route path="/home" element={<Home tableData={tableData} tableMaterial={tableMaterial} />} />
+            <Route path="/" element={<Home tableData={tableData} tableMaterial={tableMaterial} />} />
             <Route path="/cadastro" element={<Cadastro handleChange={handleChange} formInputData={formInputData} handleSubmit={handleSubmit}/>} />
             <Route path="/cadastroMP" element={<CadastroMateriaPrima onChange={onChange} formInput={formInput} onSubmit={onSubmit}/>} />
             <Route path="/producao" element={<Producao tableData={tableData} setTableData={setTableData} />} />
             <Route path="/venda" element={<Venda tableData={tableData} setTableData={setTableData} />} />
             <Route path="/relatorioproduzidos" element={<RelatorioProduzidos tableData={tableData}/>} />
-            <Route path="/relatorioMP" element={<RelatorioMateriaPrima/>} />
+            <Route path="/relatorioMP" element={<RelatorioMateriaPrima tableMaterial = {tableMaterial}/>} />
           </Routes>
        </Router>
 
-        <CadastroMateriaPrima onChange={onChange} formInput={formInput} onSubmit={onSubmit}/>
-      
-        <Cadastro handleChange={handleChange} formInputData={formInputData} handleSubmit={handleSubmit}/>
-  
-  
-  
-      <div id="produzir">
-          <Producao tableData={tableData} setTableData={setTableData} />
 
-      </div>
-
-      <Venda tableData={tableData} setTableData={setTableData} />
-  
-      <RelatorioProduzidos tableData={tableData}/>
-
-      
-
-     
           
       </main>
   </>  
