3e7c72a5a124463a9094455d2bc60735be66b267
Lucca-1999 <72893497+Lucca-1999@users.noreply.github.com>
2022-05-24 18:26:43 -0300

    Update VendaContainer.js
    Update contendo o nome das colunas do relatório de Vendas

--- src/Components/VendaContainer.js
+++ src/Components/VendaContainer.js
@@ -4,6 +4,19 @@ import VendaInfo from "./VendaInfo";
 const VendaContainer = ({tableData}) =>{
     return(
         <>
+            <table className="feedstock-cell">
+                <tr>
+                    <td>
+                        <p>Nome</p>
+                    </td>
+                    <td>
+                        <p>Quantidade Produto</p>
+                    </td>
+                    <td>
+                        <p>Preco</p>
+                    </td>
+                </tr>
+            </table>
             {tableData.map((tableData) =>(
                 <VendaInfo tableData={tableData}/>
             ))}
