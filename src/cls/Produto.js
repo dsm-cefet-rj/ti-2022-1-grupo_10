583398e3688bfdf4aee1cde73b7b29b3128d6f84
Gabriel Moraes <gabriel_themf@hotmail.com>
2022-05-31 20:17:54 -0300

    updated classes

--- src/cls/Produto.js
+++ src/cls/Produto.js
@@ -8,14 +8,24 @@ class Produto {
     aumentaVendas(){
         this.qtdVendas++;
     }
+
+    produzir(){
+        this.qtd++;
+        this.mpUseArr.forEach(this.checkUsingMateriaPrima);
+    }
+
+    checkUsingMateriaPrima(materiaPrima){
+        materiaPrima.qtd--;        
+    }
     
     // constructor
-    constructor(tipo, qtd, custo, preco, mpUsadas /* array de mp's */){
+    constructor(tipo, qtd, custo, preco, mpUseArr /* array de mp's usadas: string */, mpArr /* array de mp's cadastradas: object */){
         this.tipo = tipo;
         this.qtd = qtd;
         this.custo = custo;
         this.preco = preco;
-        this.mpUsadas = mpUsadas;
+        this.mpUseArr = mpUseArr;
+        this.mpArr = mpArr;
         this.qtdVendas = 0;
     }
 }
