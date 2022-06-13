8b39474d6ed1748376dd3d90144eb3a19d277ad1
Matheus Melo <73976198+matheussmelo@users.noreply.github.com>
2022-06-13 15:53:26 -0300

    Update LinhaProdutos.js

--- src/Components/LinhaProdutos.js
+++ src/Components/LinhaProdutos.js
@@ -1,10 +1,19 @@
+import { useDispatch } from "react-redux";
+import { Delete } from "../app/produtosSlice";
+
 const LinhaProdutos = (produto) => {
+    const dispatch = useDispatch();
+    const handleDelete = () => {
+    let payload = {id:produto.produto.id}
+    dispatch(Delete(payload))
+    }
     return ( 
         <tr>
             <td>{produto.produto.id}</td>
             <td>{produto.produto.nomeProduto}</td>
             <td>{produto.produto.custoProduto}</td>
             <td>{produto.produto.valorProduto}</td>
+            <button onClick={handleDelete} id ='deletar'>Delete</button>
         </tr>
      );
 }
