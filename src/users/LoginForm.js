f505d62f7e99de249a9e98680d6598820e320d64
mpsdo <mpsdo@yahoo.com.br>
2022-07-20 02:13:38 -0300

    feature/tela-login-css

--- src/users/LoginForm.js
+++ src/users/LoginForm.js
@@ -5,6 +5,7 @@ import { useForm } from "react-hook-form";
 import {loginServer} from './LoginSlice';
 import { ToastContainer, toast } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
+import { TextInput, Button, Group, Box, Text, Title } from '@mantine/core';
 
 
 /**
@@ -52,38 +53,73 @@ export default function LoginForm(props){
 
 
 
-    return(<>
-            <ToastContainer
-                position="top-center"
-                autoClose={5000}
-                hideProgressBar={false}
-                newestOnTop={false}
-                closeOnClick
-                rtl={false}
-                pauseOnFocusLoss
-                draggable
-                pauseOnHover
-            />
-            <h1>Login</h1>
+    return(
+    <>
+        <ToastContainer
+            position="top-center"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+        />
+        <Box sx={{ maxWidth: 300 }} mx="auto">
             <form onSubmit={handleSubmit(onSubmit)}>
-            <label>
-                Login:
-                <input type="text" name="username" id="username" {...register('username')} />
-                &nbsp;<span id="username_err_msg">{errors.login?.message}</span>
-            </label>
-            <br/>
-            <label>
-                Senha:
-                <input type="password" name="password" id="password" {...register('password')} />
-                &nbsp;<span id="password_err_msg">{errors.password?.message}</span>
-            </label>
-            <br/>
-            <br/>   
-            <button type="submit" id="Login" name="btn_login" variant="contained" color="primary">Login</button>
-            <li>
-                <NavLink to="/signup"> Criar conta </NavLink>
-            </li>
+                <Title order={1} style={{ marginTop: 20, fontWeight: 500 }}>Login</Title>
+
+                <Text align="left" mt={'md'}>Nome</Text>
+                <TextInput
+                    required
+                    sx={{marginTop:5}}
+                    class="form-control-material" 
+                    type="text" name="username"
+                    id="username" {...register('username')}
+                />
+                    &nbsp;<span id="username_err_msg">{errors.login?.message}</span>
+                
+                <Text align="left">Senha</Text>
+                <TextInput
+                    required
+                    sx={{marginTop:5}}
+                    class="form-control-material"
+                    type="password"
+                    name="password"
+                    id="password" {...register('password')}
+                />
+                    &nbsp;<span id="password_err_msg">{errors.password?.message}</span>
+
+                <Group position="center">
+                    <Button
+                        class="btn_4 btn-primary btn-ghost"
+                        type="submit" 
+                        id="Login"
+                        name="btn_login"
+                        variant="contained"
+                        color="primary"
+                    >
+                        Login
+                    </Button>
+                    <Button
+                        class="btn_4 btn-primary btn-ghost"
+                        variant="contained"
+                        color="primary"
+                    >
+                        <Text 
+                            size="xs"
+                            variant="link"
+                            component="a"
+                            href="/signup"
+                            align="left"
+                            >
+                            Criar conta
+                        </Text>
+                    </Button>
+                </Group>
             </form>
-          </>
-    );
+        </Box>
+    </>
+);
 }
