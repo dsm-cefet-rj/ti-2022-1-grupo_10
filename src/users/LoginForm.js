60f549c3f7ca7d229df2fcf8a65c6096c43c2efc
mpsdo <mpsdo@yahoo.com.br>
2022-07-20 01:56:23 -0300

    feature/tela-login-css

--- src/users/LoginForm.js
+++ src/users/LoginForm.js
@@ -3,18 +3,7 @@ import { useParams, useNavigate, NavLink } from "react-router-dom";
 import {useDispatch, useSelector} from 'react-redux';
 import { useForm } from "react-hook-form";
 import {loginServer} from './LoginSlice';
-import { ToastContainer, toast } from 'react-toastify';
-import "react-toastify/dist/ReactToastify.css";
-
-
-/**
-*@module users/LoginForm
-*/
-
-/**
- * neste componente é feito o formulário de login do site, contendo validação através de token jwt e caso um não exista usuário ou conexão com o banco de dados será feito uma notificação de erro
- * @param {Login} login - informação de usuário e senha a ser passado para validação de conexão ao site
- */
+import { TextInput, Button, Group, Box, Text, Title } from '@mantine/core';
 
 export default function LoginForm(props){
 
@@ -36,54 +25,65 @@ export default function LoginForm(props){
         if (status === 'logged_in' ) {
             navigate('/home');
         }
-        else if (status === 'failed'){
-            toast.error('Algo deu errado!', {
-                position: "top-center",
-                autoClose: 5000,
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-                progress: undefined,
-                });
-        }
     }, [status])
 
 
 
 
-    return(<>
-            <ToastContainer
-                position="top-center"
-                autoClose={5000}
-                hideProgressBar={false}
-                newestOnTop={false}
-                closeOnClick
-                rtl={false}
-                pauseOnFocusLoss
-                draggable
-                pauseOnHover
-            />
-            <h1>Login</h1>
+    return(
+        <Box sx={{ maxWidth: 300 }} mx="auto">
             <form onSubmit={handleSubmit(onSubmit)}>
-            <label>
-                Login:
-                <input type="text" name="username" id="username" {...register('username')} />
-                &nbsp;<span id="username_err_msg">{errors.login?.message}</span>
-            </label>
-            <br/>
-            <label>
-                Senha:
-                <input type="password" name="password" id="password" {...register('password')} />
-                &nbsp;<span id="password_err_msg">{errors.password?.message}</span>
-            </label>
-            <br/>
-            <br/>   
-            <button type="submit" id="Login" name="btn_login" variant="contained" color="primary">Login</button>
-            <li>
-                <NavLink to="/signup"> Criar conta </NavLink>
-            </li>
+                <Title order={1} style={{ marginTop: 20, fontWeight: 500 }}>Login</Title>
+
+                <Text align="left" mt={'md'}>Nome</Text>
+                <TextInput
+                    required
+                    sx={{marginTop:5}}
+                    class="form-control-material" 
+                    type="text" name="username"
+                    id="username" {...register('username')}
+                />
+                    &nbsp;<span id="username_err_msg">{errors.login?.message}</span>
+                
+                <Text align="left">Senha</Text>
+                <TextInput
+                    required
+                    sx={{marginTop:5}}
+                    class="form-control-material"
+                    type="password"
+                    name="password"
+                    id="password" {...register('password')}
+                />
+                    &nbsp;<span id="password_err_msg">{errors.password?.message}</span>
+
+                <Group position="center">
+                    <Button
+                        class="btn_4 btn-primary btn-ghost"
+                        type="submit" 
+                        id="Login"
+                        name="btn_login"
+                        variant="contained"
+                        color="primary"
+                    >
+                        Login
+                    </Button>
+                    <Button
+                        class="btn_4 btn-primary btn-ghost"
+                        variant="contained"
+                        color="primary"
+                    >
+                        <Text 
+                            size="xs"
+                            variant="link"
+                            component="a"
+                            href="/signup"
+                            align="left"
+                            >
+                        Criar conta
+                </Text>
+                    </Button>
+                </Group>
             </form>
-          </>
+        </Box>
     );
 }
--- src/users/SignupForm.js
+++ src/users/SignupForm.js
@@ -3,17 +3,8 @@ import { useParams, useNavigate } from "react-router-dom";
 import {useDispatch, useSelector} from 'react-redux';
 import { useForm } from "react-hook-form";
 import {signupServer} from './SignupSlice';
+import { TextInput, Button, Group, Box, Text, Title } from '@mantine/core';
 
-/**
-*@module users/SignupForm
- */
-
-/**
- * neste componente é feito o formulário de registro do site, portanto, onde será criado a conta do usuário
- * @param {Login} login - entidade a ser utilizada
- * @param {string} Usuário - nome do Usuário a ser cadastrado
- * @param {string} Senha - senha do usuário que será encriptada no banco de dados
- */
 
 export default function SignupForm(props){
 
@@ -37,27 +28,46 @@ export default function SignupForm(props){
         }
     }, [status])
 
-
-
-
-    return(<>
-            <h1>Signup</h1>
+    return(
+        <Box sx={{ maxWidth: 300 }} mx="auto">
             <form onSubmit={handleSubmit(onSubmit)}>
-            <label>
-                Usuário:
-                <input type="text" name="username" id="username" {...register('username')} />
-                &nbsp;<span id="username_err_msg">{errors.signup?.message}</span>
-            </label>
-            <br/>
-            <label>
-                Senha:
-                <input type="password" name="password" id="password" {...register('password')} />
-                &nbsp;<span id="password_err_msg">{errors.password?.message}</span>
-            </label>
-            <br/>
-            <br/>   
-            <button type="submit" id="Signup" name="btn_signup" variant="contained" color="primary">Criar Conta</button>
+                <Title order={1} style={{ marginTop: 20, fontWeight: 500 }}>Signup</Title>
+
+                <Text align="left" mt={'md'}>Usuário:</Text>
+                <TextInput
+                    required
+                    sx={{marginTop:5}}
+                    class="form-control-material" 
+                    type="text"
+                    name="username"
+                    id="username" {...register('username')}
+                    />
+                    &nbsp;<span id="username_err_msg">{errors.signup?.message}</span>
+                
+                <Text align="left">Senha:</Text>
+                <TextInput
+                    required
+                    sx={{marginTop:5}}
+                    class="form-control-material"
+                    type="password"
+                    name="password"
+                    id="password" {...register('password')}
+                    />
+                    &nbsp;<span id="password_err_msg">{errors.password?.message}</span>
+
+                <Group position="center">
+                    <Button
+                        class="btn_4 btn-primary btn-ghost"
+                        type="submit" 
+                        id="Login"
+                        name="btn_login"
+                        variant="contained"
+                        color="primary"
+                    >
+                        Criar Conta
+                    </Button>
+                </Group>
             </form>
-          </>
+        </Box>
     );
 }
