bd2345f7c3fe2cdb8cbbba6f083a0455be2c707b
mpsdo <mpsdo@yahoo.com.br>
2022-07-20 01:39:14 -0300

    feature/tela-login-css

--- src/users/LoginForm.js
+++ src/users/LoginForm.js
@@ -3,6 +3,7 @@ import { useParams, useNavigate, NavLink } from "react-router-dom";
 import {useDispatch, useSelector} from 'react-redux';
 import { useForm } from "react-hook-form";
 import {loginServer} from './LoginSlice';
+import { TextInput, Button, Group, Box, Text, Title } from '@mantine/core';
 
 export default function LoginForm(props){
 
@@ -29,27 +30,60 @@ export default function LoginForm(props){
 
 
 
-    return(<>
-            <h1>Login</h1>
+    return(
+        <Box sx={{ maxWidth: 300 }} mx="auto">
             <form onSubmit={handleSubmit(onSubmit)}>
-            <label>
-                Login:
-                <input type="text" name="username" id="username" {...register('username')} />
-                &nbsp;<span id="username_err_msg">{errors.login?.message}</span>
-            </label>
-            <br/>
-            <label>
-                Senha:
-                <input type="password" name="password" id="password" {...register('password')} />
-                &nbsp;<span id="password_err_msg">{errors.password?.message}</span>
-            </label>
-            <br/>
-            <br/>   
-            <button type="submit" id="Login" name="btn_login" variant="contained" color="primary">Login</button>
-            <li>
-                <NavLink to="/signup"> Criar conta </NavLink>
-            </li>
+                <Title order={1} style={{ marginTop: 20, fontWeight: 500 }}>Login</Title>
+
+                <Text align="left" mt={'md'}>Nome</Text>
+                <TextInput
+                    required
+                    sx={{marginTop:5}}
+                    class="form-control-material" 
+                    type="text" name="username"
+                    id="username" {...register('username')}
+                />
+                    &nbsp;<span id="username_err_msg">{errors.login?.message}</span>
+                
+                <Text align="left">Senha</Text>
+                <TextInput
+                    required
+                    sx={{marginTop:5}}
+                    class="form-control-material"
+                    type="password"
+                    name="password"
+                    id="password" {...register('password')}
+                />
+                    &nbsp;<span id="password_err_msg">{errors.password?.message}</span>
+
+                <Group position="center">
+                    <Button
+                        class="btn_4 btn-primary btn-ghost"
+                        type="submit" 
+                        id="Login"
+                        name="btn_login"
+                        variant="contained"
+                        color="primary"
+                    >
+                        Login
+                    </Button>
+                    <Button
+                        class="btn_4 btn-primary btn-ghost"
+                        variant="contained"
+                        color="primary"
+                    >
+                        <Text 
+                            size="xs"
+                            variant="link"
+                            component="a"
+                            href="/signup"
+                            align="left"
+                            >
+                        Criar conta
+                </Text>
+                    </Button>
+                </Group>
             </form>
-          </>
+        </Box>
     );
 }
